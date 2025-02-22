6fda21e469f3c9d439937d9492436cbe5485b49f
Gaurav Mohol <gaurav.github@gmail.com>
2025-02-23 00:43:42 +0530

    Add files via upload

---
+++ Repetithon/session_05/quadratic_solver_01.py
@@ -0,0 +1,25 @@
+'''
+quadratic equation :X^2 + x +c =0 
+Formula :  sqrt(-b + (b^2 -4ac)) / 2*a
+'''
+
+import sys
+
+a = float(input('Enter CF of x^2: '))
+b = float(input('Enter the CF of x: '))
+c = float(input('Enter the constant of eqn'))
+
+if a== 0.0:
+    print('CF of x^2 cannot be zero')
+    sys.exit()
+
+
+if b**2 - (4 * a*c) < 0.0:
+    
+    print('This eq. does not have in real numbers')
+    sys.exit()
+
+r1 = (-b + (b**2 - 4*a*c) ** 0.5) /(2*a)
+r2 = (-b -(b**2 - 4*a*c) ** 0.5) /(2*a)
+
+print(f'root1: {r1}, Root: {r2}')
---
+++ Repetithon/session_05/quadratic_solver_03.py
@@ -0,0 +1,17 @@
+
+
+import sys
+
+a = float(input('Enter CF of x^2: '))
+b = float(input('Enter the CF of x: '))
+c = float(input('Enter the constant of eqn'))
+
+if a== 0.0:
+    print('CF of x^2 cannot be zero')
+    sys.exit()
+if b**2 - (4 * a*c) < 0.0:
+    print('This eq. does not have in real numbers')
+    sys.exit()
+r1 = (-b + (b**2 - 4*a*c) ** 0.5) /(2*a)
+r2 = (-b -(b**2 - 4*a*c) ** 0.5) /(2*a)
+print(f'root1: {r1}, Root: {r2}')
